aec2a0465034049895fd5c6416fcd5540d8a10fd
anyidong <anyidong1993@163.com>
2023-03-13 15:45:40 +0800

    更新项目增加status字段

--- app/labeler/model/project.go
+++ app/labeler/model/project.go
@@ -6,5 +6,6 @@ type Project struct {
 	ID       primitive.ObjectID `bson:"_id" json:"id"`
 	Name     string             `bson:"name" json:"name"`
 	FolderID primitive.ObjectID `bson:"folderId" json:"folderId"`
+	Status   string             `bson:"status" json:"status"`
 	Schema   Schema             `bson:"schema" json:"schema"`
 }
